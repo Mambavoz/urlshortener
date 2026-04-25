f20b178d0d3e77935dd1d147a2aea97e4cc26661
Mambavoz <sirdknv@gmail.com>
2026-04-25 19:51:00 +0300

    logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,21 +1,46 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"urlshort/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	// TODO: init config: cleanenv
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
-
 	// TODO: init logger: log/slog
+	log := setupLogger(cfg.Env)
+
+	// или log = log.With(slog.String("env", cfg.Env)) и дальше юзать log.Info, log.Debug, log.Error
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
 	// TODO: init storage: sqlite
 
 	// TODO: init router: chi совместим с net/http, "chi render"
 
 	// TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
